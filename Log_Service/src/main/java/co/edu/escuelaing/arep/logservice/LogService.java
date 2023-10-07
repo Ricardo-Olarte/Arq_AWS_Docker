1bfdb54946095130d8a00fa538df8ef71e7a5afa
Ricardo Olarte <olarte.jose.r@gmail.com>
2023-10-06 20:27:40 -0500

    cuerpo de conexion de con Mongo

--- Log_Service/src/main/java/co/edu/escuelaing/arep/logservice/LogService.java
+++ Log_Service/src/main/java/co/edu/escuelaing/arep/logservice/LogService.java
@@ -1,6 +1,6 @@
 package co.edu.escuelaing.arep.logservice;
 
-import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -9,7 +9,11 @@ import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Sorts;
 
 import org.bson.Document;
+import org.bson.json.JsonObject;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 import static spark.Spark.*;
 
@@ -36,12 +40,39 @@ public class LogService {
     }
 
     private static String logMessage(String string){
-        return """
-                    {
-                    "m1": "message1"
-                    "m2": "message2"
-                    "m3": "message3"
-                    }
-               """;
+        return string;
     }
+
+    private static List<Document> connectionMongo(String string){
+        String uriDB = "mongodb://mongodb:27017";
+
+        MongoClient mongoClient = MongoClients.create(uriDB);
+        MongoDatabase database = mongoClient.getDatabase("arep-mongoDB");
+        MongoCollection<Document> collection = database.getCollection("message");
+        List<Document> documents = new ArrayList<>();
+        try(MongoCursor<Document> mongoCursor = collection.find().limit(10).sort(Sorts.descending("date")).iterator()){
+            while (mongoCursor.hasNext()){
+                documents.add(mongoCursor.next());
+            }
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        return documents;
+    }
+
+
+    private static void saveValue(String string){
+        MongoClient mongoClient = MongoClients.create("mongodb://mongodb:27017");
+        MongoDatabase database = mongoClient.getDatabase("arep-mongoDB");
+        MongoCollection<Document> collection = database.getCollection("logs");
+
+        String currentDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+        Document document = new Document("string", string).append("date", currentDate);
+
+        collection.insertOne(document);
+        mongoClient.close();
+    }
+
 }
